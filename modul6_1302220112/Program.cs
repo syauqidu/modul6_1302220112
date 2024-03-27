8ac946fbb7884038d6d47228864050275c4bcab3
syauqidu <syauqi.d.u@gmail.com>
2024-03-27 07:15:44 +0700

    first commit

---
+++ modul6_1302220112/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace modul6_1302220112
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
